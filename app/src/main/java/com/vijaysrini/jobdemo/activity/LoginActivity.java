baa415357ac7bc834f588af23cacb1426a313618
VijaySrinivasan <vijay.k.srinivasan@gmail.com>
2016-01-24 16:39:14 -0500

    Changed login id and pwd

--- app/src/main/java/com/vijaysrini/jobdemo/activity/LoginActivity.java
+++ app/src/main/java/com/vijaysrini/jobdemo/activity/LoginActivity.java
@@ -43,7 +43,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "jay@ClinicalInk.com:craig", "vijay@ClinicalInk.com:srini"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
